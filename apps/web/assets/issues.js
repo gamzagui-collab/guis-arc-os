ec2952f53e7b489d4c1ca0b99758995a2d0c132f
GAMZAGUI <gamzagui@gmail.com>
2026-08-13 17:08:31 +0900

    chore: finalize location import static revision

--- apps/web/assets/issues.js
+++ apps/web/assets/issues.js
@@ -1,5 +1,5 @@
 import {escapeHtml,emptyState,errorState,loadingSkeleton,statusBadge} from "../../../packages/ui/components.js";
-import {canonicalLocationId,extractLocationSpeech,naturalLocationSort} from "./issue-speech.js?v=0.27.1-r1";
+import {canonicalLocationId,extractLocationSpeech,naturalLocationSort} from "./issue-speech.js?v=0.27.1-r2";
 import {DEPARTMENT_LABELS,departmentLabel,errorMessage,issueHistoryReasonLabel} from "./i18n/ko.js";
 import {setActionPending,clearActionPending,showActionSuccess,showActionError} from "./action-feedback.js";
 import {issueActionVisibility} from "./issue-action-policy.js?v=0.27.0";
